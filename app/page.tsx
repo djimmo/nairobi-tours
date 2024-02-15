a81a2a381f9c9095d4707edac2529d9e54497ec5
Djim Molenkamp <djimmo@gmail.com>
2024-02-15 20:14:25 +0000

    simplified page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 {/*       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"> */}
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="flex w-full justify-center border-gray-300 p-4">
 {/*           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code> */}
         <p>
@@ -15,15 +15,17 @@ export default function Home() {
           - Unique way also on Slum tour! <br/> <br/>
           Contact me on +254706404264 <br/><br/>
         </p>
+        </div>
+        
         <Image
-          className="flex"
+          className="f"
           src="/dan.jpeg"
           alt="Dan Cox"
           width={270}
           height={480}
           priority
-        />
-        </div>
+        />  
+        
 {/*         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
